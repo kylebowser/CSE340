f3f72c2af3e7498d70d6e566ef685102dbca559b
kylebowser <kylebowser9@gmail.com>
2026-03-27 21:36:25 -0600

    Week 4 activities

---
+++ routes/accountRoute.js
@@ -0,0 +1,19 @@
+// Needed Resources 
+const express = require("express")
+const router = new express.Router() 
+const utilities = require("../utilities/index")
+const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
+
+
+//Route for account login screen
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
+
+//Route for the signup screen
+router.get("/signup",utilities.handleErrors(accountController.buildSignup))
+
+//Route for the post of signup
+router.post('/signup', regValidate.registationRules(), regValidate.checkRegData, utilities.handleErrors(accountController.registerAccount))
+
+
+module.exports = router;
